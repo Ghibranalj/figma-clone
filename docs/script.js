149eab461aba07b3e558bd0487aa5aa423599895
Ghibranalj <24712554+Ghibranalj@users.noreply.github.com>
2022-06-01 14:44:23 +0200

    Added github pages

---
+++ docs/script.js
@@ -0,0 +1,48 @@
+
+
+let setColor;
+let takeScreenshot;
+let toggleEraser;
+
+Module.onRuntimeInitialized = function() {
+    console.log("Module loaded");
+    setColor = Module.cwrap('setColor', null, ['string']);
+    takeScreenshot = Module.cwrap('makeScreenshot', null, ['string']);
+    toggleEraser = Module.cwrap('toggleEraser', null, []);
+}
+
+
+
+function buttonColorChange(col){
+    setColor(col);
+}
+
+function takeScreenShot(){
+    const time = new Date().getTime();
+    takeScreenshot("screenshot-"+ time);
+}
+
+
+function saveFileFromMEMFSToDisk(memoryFSname, localFSname)     // This can be called by C/C++ code
+{
+    let isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+    let data = FS.readFile(memoryFSname);
+    let blob;
+
+    if (isSafari) blob = new Blob([data.buffer], { type: "image/png" });
+    else blob = new Blob([data.buffer], { type: "image/png" });
+
+    let link = window.URL.createObjectURL(blob);
+    download(link);
+}
+
+function download(url){
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = "screenshot.png";
+    link.click();
+}
+
+function tEraser(){
+    toggleEraser();
+}
