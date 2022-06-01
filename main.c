2d40f2fce0e599ce81e6d0f82bd264d8f66602c9
Ghibranalj <24712554+Ghibranalj@users.noreply.github.com>
2022-06-01 14:21:24 +0200

    added drawing rectangle and screenshot

--- main.c
+++ main.c
@@ -1,42 +1,111 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
 
 #include "raylib.h"
 
-int main(void)
-{
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+#define MAX_REC 255
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+Color col;
+bool shouldTakeScreenshot = false;
+char *screenshotLocation;
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+typedef struct {
+  Rectangle rec;
+  Color col;
+} myRectangle;
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
+int main(void) {
 
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
+  myRectangle rect[MAX_REC];
+  int count = 0;
+  col = BLACK;
 
-            ClearBackground(RAYWHITE);
+  const int screenWidth = 800;
+  const int screenHeight = 450;
+  SetTraceLogLevel(LOG_NONE);
+  InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+  SetTargetFPS(60);
+  Vector2 from;
+  while (!WindowShouldClose()) {
 
-        EndDrawing();
-        //----------------------------------------------------------------------------------
+    BeginDrawing();
+
+    ClearBackground(RAYWHITE);
+    for (int i = 0; i < count; i++) {
+      Rectangle rec = rect[i].rec;
+      Color col = rect[i].col;
+      DrawRectangle(rec.x, rec.y, rec.width, rec.height, col);
+    }
+    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+      from = GetMousePosition();
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+    if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
 
-    return 0;
+      Vector2 to = GetMousePosition();
+      Rectangle rec = (Rectangle){from.x, from.y, to.x - from.x, to.y - from.y};
+
+      DrawRectangle(rec.x, rec.y, rec.width, rec.height, col);
+    }
+
+    if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
+      Vector2 to = GetMousePosition();
+      rect[count].rec =
+          (Rectangle){from.x, from.y, to.x - from.x, to.y - from.y};
+      rect[count].col = col;
+      count++;
+    }
+
+    EndDrawing();
+
+    if (shouldTakeScreenshot) {
+      shouldTakeScreenshot = false;
+      TakeScreenshot("test.png");
+    }
+  }
+  CloseWindow(); // Close window and OpenGL context
+
+  return 0;
+}
+
+void makeScreenshot(char *filename) {
+  shouldTakeScreenshot = true;
+  screenshotLocation = filename;
+  strcpy(screenshotLocation, filename);
+}
+
+void setColor(char *color) {
+
+  if (strcasecmp(color, "black") == 0) {
+    col = BLACK;
+    return;
+  }
+
+  if (strcasecmp(color, "white") == 0) {
+    col = WHITE;
+    return;
+  }
+
+  if (strcasecmp(color, "red") == 0) {
+    col = RED;
+    return;
+  }
+
+  if (strcasecmp(color, "green") == 0) {
+    col = GREEN;
+    return;
+  }
+
+  if (strcasecmp(color, "blue") == 0) {
+    col = BLUE;
+    return;
+  }
+
+  col = YELLOW;
+
+  free(color);
 }
--- makefile
+++ makefile
@@ -5,11 +5,15 @@ SHELL_HTML=index.html
 INC= vendor/raylib_wasm/include
 LIB= vendor/raylib_wasm/libraylib.a
 
+EXPORTED_FUNC = test
+
 all: docs
-	$(CC) main.c -o docs/index.html -I$(INC) -s USE_GLFW=3\
-	  --shell-file $(SHELL_HTML) $(LIB_PARAMS) $(LIB) -L$(LIB)
+	$(CC) main.c -o docs/index.html -I$(INC) -sUSE_GLFW=3 -sASYNCIFY\
+	  --shell-file $(SHELL_HTML) $(LIB_PARAMS) $(LIB) -L$(LIB)\
+	   -s EXPORTED_FUNCTIONS=_makeScreenshot,_setColor,_main -s 'EXPORTED_RUNTIME_METHODS=ccall,cwrap'
 docs:
 	mkdir docs
+	cp script.js docs
 
 .PHONY: clean
 
